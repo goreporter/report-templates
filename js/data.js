88cd7391093b4267784477f1393d6d437e2dbe32
刘菲菲 <liufeifei@b.360.cn>
2017-09-08 20:42:19 +0800

    给data文件写注释

--- js/data.js
+++ js/data.js
@@ -1,18 +1,18 @@
 	//data
 		var resData = {
-				"score": 37,
-				"issueNum": 52,   //问题总数
-				"gotest":{
-					summary:{
+				"score": 37,    //首页部分的“分数”一栏
+				"issueNum": 52,   //首页部分的“Issues"一栏：问题总数。
+				"gotest":{        //单元测试页面的数据
+					summary:{      //单元测试页面的上部分总结部分
 						"codeCover": '75',     //代码覆盖率
 						"pkgCover":  '80',     //包覆盖率
 						"totalTime":  '0.874'    //单元测试耗时
 					},
 					content:{
 						pkg: ["/gocode", "/gocode/dupl/output", "/gotest", "/gocode/copycode/output", "/gocode/dupl/suffixtree", "/gosimple/gotool", "/gocode/dupl/syntax", "/gocyclo", "/godep", "/gosimple/lint", "/gocode/deadcode", "/gosimple/simple"],
-						time: [0.04, 0.009, 0.505, 0.008, 0.09, 0.09, 0.008, 0.01, 0.101, 0.013, 0, 0],
-						cover: [49.4, 23.4, 65.2, 22, 90.6, 45.7, 55.8, 66.7, 70.4, 6.2, 0, 0],
-						noTest: ["/gocheck", "/gocheck/structcheck", "/gocheck/structcheck"]
+						time: [0.04, 0.009, 0.505, 0.008, 0.09, 0.09, 0.008, 0.01, 0.101, 0.013, 0, 0],   //每个包的耗时，位置跟pkg数组里的包相对应。
+						cover: [49.4, 23.4, 65.2, 22, 90.6, 45.7, 55.8, 66.7, 70.4, 6.2, 0, 0],    //每个包的覆盖率，位置跟pkg数组里的包对应。
+						noTest: ["/gocheck", "/gocheck/structcheck", "/gocheck/structcheck"]   //没有单元测试代码的包。
 					}
 				},
 				//代码风格
@@ -26,12 +26,20 @@
 						gofmt: {
 							label: 'Gofmt formats Go programs. We run gofmt -s on your code, where -s is for the "simplify" command',
 							score: '100%',
-							detail: []
+							detail: [{
+											rep: 'Sia/compatibility/siag_1.0_test.go',   //存在问题的路径
+											content: [ "Line 18: warning: don't use underscores in Go names; type KeyPairSiag_1_0 should be KeyPairSiag1_0 (golint)",
+													 "Line 28: warning: don't use underscores in Go names; func verifyKeysSiag_1_0 should be verifyKeysSiag1_0 (golint)"
+											]  //需要显示的每一行
+										},{
+											rep: 'Sia/types/target.go',
+											content: ["Line 24: warning: exported var ErrNegativeTarget should have comment or be unexported (golint)","Line 46: warning: receiver name t should be consistent with previous receiver name x for Target (golint)"]
+										}]   
 						},
 						go_vet: {
 							label: 'go vet examines Go source code and reports suspicious constructs, such as Printf calls whose arguments do not align with the format string.',
 							score: '100%',
-							detail: []
+							detail: []   //这个部分的每个detail字段都遵循上个detail的数据格式
 						},
 						golint: {
 							label: 'Golint is a linter for Go source code.',
